74a765a4ef827576002a630a91d7a7992f025dfd
Fitsum Gaim <fitsum@geezlab.com>
2018-11-14 17:19:42 +0900

    Add config template

---
+++ config.sample.py
@@ -0,0 +1,10 @@
+class Config:
+
+    MONGODB_SETTINGS = {
+        'host': '0.0.0.0',
+        'db': 'YOUR_DB_NAME',
+        'port': 27017,
+        'username': 'YOUR_DB_USERNAME',
+        'password': 'YOUR_DB_PASSWORD',
+        'authentication_source': 'admin'
+    }
