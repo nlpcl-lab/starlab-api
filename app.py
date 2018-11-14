c34a2691443791f99a75f5bdcec24c7c93239719
Fitsum Gaim <fitsum@geezlab.com>
2018-11-14 18:21:27 +0900

    Add backend api for artext

--- app.py
+++ app.py
@@ -33,6 +33,32 @@ def log_api_access(key):
     log.save()
 
 
+
+# APIs
+
+# artext
+@app.route('/artext')
+def artext_view():
+    return render_template('api_artext.html')
+
+
+from artext import Artext
+artxt = Artext()
+artxt.samples = 10
+artxt.error_overall = 0.25
+
+@app.route('/api/artext/noise', methods=['POST'])
+def artext_process():
+    log_api_access('artext')
+
+    data = request.get_json()
+    doc = data['input']
+    noises = artxt.noise_document(doc)
+
+    response = jsonify({'input': doc, 'output': '\n'.join(noises)})
+    return response
+
+
 if __name__ == '__main__':
     base_dir = os.path.abspath(os.path.dirname(__file__) + '/')
     sys.path.append(base_dir)
